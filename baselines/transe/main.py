20408af55527f5da15d9528ba725e489ed686d41
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-10 23:53:45 +0000

    TranE running

---
+++ baselines/transe/main.py
@@ -0,0 +1,46 @@
+from baselines.transe.trainer import Trainer
+from baselines.transe.tester import Tester
+from baselines.transe.model import TransE
+from baselines.transe.negative_sampling import NegativeSampling
+from baselines.transe.loss import MarginLoss
+from data import TrainDataLoader, TestDataLoader
+
+# dataloader for training
+train_dataloader = TrainDataLoader(
+	in_path = "./data/FB15K237/",
+	nbatches = 100,
+	threads = 8,
+	sampling_mode = "normal",
+	bern_flag = 1,
+	filter_flag = 1,
+	neg_ent = 25,
+	neg_rel = 0)
+
+# dataloader for test
+test_dataloader = TestDataLoader("./data/FB15K237/", "link")
+
+# define the model
+transe = TransE(
+	ent_tot = train_dataloader.get_ent_tot(), #entity total
+	rel_tot = train_dataloader.get_rel_tot(), # relation total
+	dim = 200,
+	p_norm = 1,
+	norm_flag = True)
+
+
+# define the loss function
+model = NegativeSampling(
+	model = transe,
+	loss = MarginLoss(margin = 5.0),
+	batch_size = train_dataloader.get_batch_size()
+)
+
+# train the model
+trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 1000, alpha = 1.0, use_gpu = True)
+trainer.run()
+transe.save_checkpoint('./checkpoint/transe.ckpt')
+
+# test the model
+transe.load_checkpoint('./checkpoint/transe.ckpt')
+tester = Tester(model = transe, data_loader = test_dataloader, use_gpu = True)
+tester.run_link_prediction(type_constrain = False)
---
+++ data/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .data_loader import TrainDataLoader
+from .data_loader import TestDataLoader
+
+__all__ = [
+	'TrainDataLoader',
+	'TestDataLoader'
+]
